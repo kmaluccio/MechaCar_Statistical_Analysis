4acea48e1bc1d8ae6072a6ba764b6e2297130559
kmaluccio <kmaluccio@tamu.edu>
2022-03-30 15:42:53 -0500

    deliverable 2 scripts complete in R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,11 @@ mechaCarData <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors =
 mechaLinReg <- lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=mechaCarData)
 # generate summary statistics
 summary(mechaLinReg)
+
+# import and read the data as a table
+suspension_table <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+# create summaries for the table data
+total_summary <- suspension_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+print(total_summary)
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+print(lot_summary) # need to adjust to print the decimals here
